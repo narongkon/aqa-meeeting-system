c7a6df7aed31d37dd82635d080e5fd6ea5cfc52e
narongkon <s5602041620230@email.kmutnb.ac.th>
2017-09-11 19:20:07 +0700

    update

--- backend/app/routes/meeting_project.routes.js
+++ backend/app/routes/meeting_project.routes.js
@@ -1,13 +1,13 @@
 module.exports = function(app){
     var meeting = require('../controllers/meeting/meeting_project.controller')
-    app.get('/project',meeting.list)
-    app.get('/project/bygroup/:id',meeting.listbygroup)
-    app.get('/project/bylevel/:group/:level',meeting.listbylevel)
-    app.get('/project/group',meeting.group)
-    app.get('/project/level',meeting.level)
-    app.get('/project/:id',meeting.select)
-    app.post('/project',meeting.add)
-    app.put('/project',meeting.edit)
-    app.delete('/project/:id',meeting.del)
+    app.get('',meeting.list)
+    app.get('/bygroup/:id',meeting.listbygroup)
+    app.get('/bylevel/:group/:level',meeting.listbylevel)
+    app.get('/group',meeting.group)
+    app.get('/level',meeting.level)
+    app.get('/:id',meeting.select)
+    app.post('',meeting.add)
+    app.put('',meeting.edit)
+    app.delete('/:id',meeting.del)
 }
 
