75ff145417d83f18b3d2a00b232440b33a5a5f8e
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-26 19:30:41 +0700

    meeting/bygroup?id=B

--- backend/app/controllers/meeting.controller.js
+++ backend/app/controllers/meeting.controller.js
@@ -15,3 +15,20 @@ exports.list = (req, res) => {
             res.status(500).send(err);
         })
 }
+exports.byGroup = function (req, res) {
+    const r = req.r;
+    r.table('meeting').getAll(req.query.id, { index: 'group_meeting_id' })
+        .eqJoin('group_meeting_id', r.table('group_meeting')).without({ right: 'id' }).zip()
+        .merge(function (m) {
+            return {
+                level: r.branch(m('group_work_id').eq(null), null, r.db('aqa_expert').table('group_work').get(m('group_work_id')))
+            }
+        })
+        .run()
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        })
+}
--- backend/app/routes/meeting.routes.js
+++ backend/app/routes/meeting.routes.js
@@ -1,4 +1,5 @@
 module.exports = function (app) {
     var controller = require('../controllers/meeting.controller')
     app.get('/', controller.list);
+    app.get('/bygroup',controller.byGroup);
 }
